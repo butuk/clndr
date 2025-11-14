85ec1cf4e7cfcd9090e30beeeece67e7544fe152
Vitali Yanusheuski <bootook@gmail.com>
2025-11-14 10:44:39 +0100

    refactor: make universal button class

--- src/ts/Button.ts
+++ src/ts/Button.ts
@@ -1,14 +1,45 @@
 import State from "./State.ts";
+import { createElement } from "./helpFunctions.js";
 
 export class Button {
   protected state = State.getInstance();
-  private element: HTMLElement | SVGElement;
+  private clickHandler?: () => void;
+  // private element: HTMLElement | SVGElement;
 
-  constructor(element: HTMLElement | SVGElement) {
+  /*constructor(element: HTMLElement | SVGElement) {
     this.element = element;
     this.onClickAction = this.onClickAction.bind(this);
     this.element?.addEventListener("click", this.onClickAction);
+  }*/
+  constructor(
+    place: HTMLElement | SVGElement,
+    content: string,
+    el: string,
+    className?: string | undefined,
+    id?: string | undefined,
+    onClick?: () => void,
+  ) {
+    this.clickHandler = onClick;
+    this.render(place, content, el, className, id);
+    this.onClickAction = this.onClickAction.bind(this);
   }
 
-  protected onClickAction() {}
+  protected render(
+    place: HTMLElement | SVGElement,
+    content: string,
+    el: string,
+    className?: string | undefined,
+    id?: string | undefined,
+  ): void {
+    const element = createElement(el, className, id);
+    element.innerHTML = content;
+    place.append(element);
+    element?.addEventListener("click", this.onClickAction);
+  }
+
+  onClickAction(): void {
+    if (this.clickHandler) {
+      this.clickHandler();
+    }
+  }
 }
--- src/ts/Header.ts
+++ src/ts/Header.ts
@@ -1,40 +1,42 @@
-import { IndicatorForYear } from "./IndicatorForYear.js";
-import { ButtonPlus } from "./ButtonPlus.js";
-import { ButtonMinus } from "./ButtonMinus.js";
-import { IndicatorButtonReset } from "./IndicatorButtonReset.js";
-import { createElement } from "./helpFunctions.js";
+import { IndicatorForYear } from "./IndicatorForYear.ts";
+import { ButtonPlus } from "./ButtonPlus.ts";
+import { ButtonMinus } from "./ButtonMinus.ts";
+import { IndicatorButtonReset } from "./IndicatorButtonReset.ts";
+import { createElement } from "./helpFunctions.ts";
+import { Button } from "./Button.ts";
 
 export class Header {
   constructor() {
     // Create elements
     const header: HTMLElement | SVGElement = createElement("header", "header");
-    const buttonMinus: HTMLElement | SVGElement = createElement(
+
+    /*const buttonMinus: HTMLElement | SVGElement = createElement(
       "span",
       "buttonMinus",
       "minus",
     );
-    buttonMinus.innerHTML = "−";
+    buttonMinus.innerHTML = "−";*/
 
     const appHeader: HTMLElement | SVGElement = createElement(
       "h1",
       "app-header",
       "year",
     );
-    const buttonPlus: HTMLElement | SVGElement = createElement(
+    /*const buttonPlus: HTMLElement | SVGElement = createElement(
       "span",
       "buttonPlus",
       "plus",
     );
-    buttonPlus.innerHTML = "+";
+    buttonPlus.innerHTML = "+";*/
 
     // Make elements work --------- !REDO
-    new ButtonMinus(buttonMinus);
+    // new ButtonMinus(buttonMinus);
     new IndicatorForYear(appHeader);
-    new ButtonPlus(buttonPlus);
+    // new ButtonPlus(buttonPlus);
 
-    header.appendChild(buttonMinus);
+    // header.appendChild(buttonMinus);
     header.appendChild(appHeader);
-    header.appendChild(buttonPlus);
+    // header.appendChild(buttonPlus);
     document.body.append(header);
 
     const buttonReset: HTMLElement | null = document.querySelector("#reset");
